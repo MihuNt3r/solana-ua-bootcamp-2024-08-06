47203825a2f64af696dce2ae84b5ace546f7ca79
MihuNt3r <borovykmis@gmail.com>
2024-08-11 11:41:48 +0000

    Finish load_keypair.ts

--- rust-exercises/load_keypair/src/main.rs
+++ rust-exercises/load_keypair/src/main.rs
@@ -1,10 +1,23 @@
 use dotenv::dotenv;
 use std::env;
+use solana_sdk::signer::keypair::Keypair;
+use solana_sdk::signer::Signer;
 
 fn main() {
     dotenv().ok();
 
-    let secret_key = env::var("SECRET_KEY").unwrap();
+    let secret_key_str = env::var("SECRET_KEY").unwrap();
 
-    println!("Secret key: {}", secret_key);
+    let trimmed = secret_key_str.trim_matches(|c| c == '[' || c == ']');
+
+    let secret_key_arr: Vec<u8> = trimmed
+        .split(',')
+        .map(|s| s.trim().parse().expect("Failed to parse number"))
+        .collect();
+
+    let keypair = Keypair::from_bytes(&secret_key_arr).unwrap();
+
+    let public_key = keypair.pubkey();
+
+    println!("Public key: {:?}", public_key);
 }
